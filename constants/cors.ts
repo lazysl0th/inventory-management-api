a27700092ed0a746c83872becbdf602463bffd80
lazy_sl0th <anton.vlrch.ch@gmail.com>
2026-05-19 16:35:09 +0400

    fix cors settings

--- constants/cors.ts
+++ constants/cors.ts
@@ -3,7 +3,7 @@ import { FRONTEND_URL } from "./base.js";
 
 export const CORS_OPTIONS: CorsOptions = {
   origin: [FRONTEND_URL],
-  methods: ['GET,HEAD,PUT,PATCH,POST,DELETE'],
+  methods: ['GET','HEAD','PUT','PATCH','POST','DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
   optionsSuccessStatus: 200
