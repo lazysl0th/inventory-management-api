50dd24831de97da80ffe768921b97eb9d9ef3d4e
lazy_sl0th <anton.vlrch.ch@gmail.com>
2026-06-15 17:39:20 +0400

    refactor(env): add env variables config, IConfig interface, reg to container

---
+++ src/infrastructure/config/env.ts
@@ -0,0 +1,20 @@
+import {
+  envSchema,
+  NodeEnv,
+  type TConfig,
+} from "#/application/interfaces/IConfig.js";
+
+const env = {
+  NODE_ENV: process.env.NODE_ENV ?? NodeEnv.Development,
+  PORT: process.env.PORT ?? 3001,
+  //SHUTDOWN_SERVER_TIMEOUT: process.env.SHUTDOWN_SERVER_TIMEOUT ?? 10000,
+  /*DATABASE_URL:
+    process.env.DATABASE_URL ??
+    'postgresql://postgres:root@localhost:5433/worklog_db?schema=public',*/
+  FRONTEND_URL: process.env.FRONTEND_URL ?? "http://localhost:8080",
+  BACKEND_URL: process.env.BACKEND_URL ?? "http://localhost:3001",
+};
+
+const config: TConfig = envSchema.parse(env);
+
+export default config;
