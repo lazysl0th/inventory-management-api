15f2bf853d4c55f0bcb93ba754ead6eccd88db06
lazy_sl0th <anton.vlrch.ch@gmail.com>
2026-06-15 22:41:41 +0400

    fix(repo): fix import path

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import "reflect-metadata";
 
-import createContainer from "#/infrastructure/config/container.js";
+import createContainer from "./infrastructure/config/container.js";
 import { container } from "tsyringe";
 import TerminusService from "./infrastructure/services/TerminusService.js";
 
