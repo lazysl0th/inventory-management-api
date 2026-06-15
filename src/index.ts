8cf3c415486793ab7dce5ced7564f4d1566db151
lazy_sl0th <anton.vlrch.ch@gmail.com>
2026-06-15 16:50:50 +0400

    refactor(container): add src folder, index.ts and container.ts

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import "dotenv/config";
+import "reflect-metadata";
+
+import createContainer from "#/infrastructure/config/container.js";
+
+createContainer();
---
+++ src/infrastructure/config/container.ts
@@ -0,0 +1,3 @@
+const createContainer = () => {};
+
+export default createContainer;
